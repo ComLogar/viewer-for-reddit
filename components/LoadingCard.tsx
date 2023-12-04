52ac46b7bc727f0a3e161c29419659fbc12991f6
ComLogar <147499559+ComLogar@users.noreply.github.com>
2023-12-04 16:05:14 +0300

    Update LoadingCard.tsx

--- components/LoadingCard.tsx
+++ components/LoadingCard.tsx
@@ -4,9 +4,7 @@ import {AspectRatio, Card, Flex, Skeleton} from '@mantine/core'
 export default function LoadingCard() {
   return (
     <Card className={classes.card}>
-      <AspectRatio ratio={3 / 2}>
         <Skeleton animate={false} height="100%" width="100%" />
-      </AspectRatio>
       <Card.Section p="md">
         <Flex justify="center" align="center">
           <Skeleton animate={false} height={24} width="80%" />
